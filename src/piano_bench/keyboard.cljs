8eda71b15b2d8d9752e7d067c64ad956f1db875d
Thomas Stephens <spiralman@gmail.com>
2020-02-29 14:45:46 -0600

    Correctly center the popup over the black keys

--- src/piano_bench/keyboard.cljs
+++ src/piano_bench/keyboard.cljs
@@ -40,26 +40,29 @@
 (defn black-key [start-key on-press white-key]
   (let [name (black-key-name white-key)
         x-off (- (* (- (inc white-key) start-key) white-width)
-                 (/ black-width 2))]
+                 (/ black-width 2))
+        center (+ x-off (/ black-width 2))]
     [:g {:key name
          :transform (str "translate(" x-off ")")
          :stroke "#000000" :stroke-width "1" :fill "#000000"
-         :on-click #(on-press x-off name)}
+         :on-click #(on-press center name)}
      [:path {:d "M0,0 L8.35795057e-07,203.122781 C8.44881404e-07,205.33103 1.78205875,207.12117 3.99814921,207.12117 L29.1475731,207.12117 C31.3556894,207.12117 33.1457214,205.330596 33.1457214,203.122781 L33.1457206,0 L0,0 Z"}]]))
 
 (defn white-key [start-key on-press key]
   (let [name (white-key-name key)
-        x-off (* (- key start-key) white-width)]
+        x-off (* (- key start-key) white-width)
+        center (+ x-off (/ white-width 2))]
     [:g {:key name
          :transform (str "translate(" x-off ")")
          :stroke "#000000" :stroke-width "1" :fill "#FFFFFF"
-         :on-click #(on-press x-off name)}
+         :on-click #(on-press center name)}
      [:path {:d "M0,324.998255 C0,327.760984 2.24331036,330.000618 5.00408559,330.000618 L64.1897584,330.000618 C66.9534386,330.000618 69.193844,327.761793 69.193844,324.998255 L69.193844,0 L0,0 L0,324.998255 Z"}]]))
 
-(defn popup [x-off key-name]
-  [:g {:transform (str "translate(" x-off ",-81)")
-       :fill "#00FF00"}
-   [:path {:d "M8.00456499,0 L61.1854375,0 C65.6062366,0 69.1900024,3.57425806 69.1900024,8.00456499 L69.1900024,61.1854375 C69.1900024,65.6062366 65.6157444,69.1900024 61.1854375,69.1900024 L45.4916992,69.1900024 L34.8994141,80.8984375 L24.3071289,69.1900024 L8.00456499,69.1900024 C3.58376582,69.1900024 0,65.6157444 0,61.1854375 L0,8.00456499 C0,3.58376582 3.57425806,0 8.00456499,0 Z"}]])
+(defn popup [center key-name]
+  (let [x-off (- center (/ white-width 2))]
+    [:g {:transform (str "translate(" x-off ",-81)")
+         :fill "#00FF00"}
+     [:path {:d "M8.00456499,0 L61.1854375,0 C65.6062366,0 69.1900024,3.57425806 69.1900024,8.00456499 L69.1900024,61.1854375 C69.1900024,65.6062366 65.6157444,69.1900024 61.1854375,69.1900024 L45.4916992,69.1900024 L34.8994141,80.8984375 L24.3071289,69.1900024 L8.00456499,69.1900024 C3.58376582,69.1900024 0,65.6157444 0,61.1854375 L0,8.00456499 C0,3.58376582 3.57425806,0 8.00456499,0 Z"}]]))
 
 (defn keyboard [start-key pressed on-press]
   (println "hello? 2" on-press)
