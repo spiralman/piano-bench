8a4173af09ba85a9c7d3cdfd11015834d7c78f21
Thomas Stephens <spiralman@gmail.com>
2020-02-29 13:37:19 -0600

    Pre-compute some stuff, so it can be used for the popup

--- src/piano_bench/core.cljs
+++ src/piano_bench/core.cljs
@@ -42,17 +42,23 @@
      (octave white-key))))
 
 (defn black-key [start-key white-key]
-  [:g {:transform (str "translate(" (- (* (- (inc white-key) start-key) white-width)
-                                       (/ black-width 2)) ")")
-       :stroke "#000000" :stroke-width "1" :fill "#000000"
-       :on-click #(println (black-key-name white-key))}
-   [:path {:d "M0,0 L8.35795057e-07,203.122781 C8.44881404e-07,205.33103 1.78205875,207.12117 3.99814921,207.12117 L29.1475731,207.12117 C31.3556894,207.12117 33.1457214,205.330596 33.1457214,203.122781 L33.1457206,0 L0,0 Z"}]])
+  (let [name (black-key-name white-key)
+        x-off (- (* (- (inc white-key) start-key) white-width)
+                 (/ black-width 2))]
+    [:g {:key name
+         :transform (str "translate(" x-off ")")
+         :stroke "#000000" :stroke-width "1" :fill "#000000"
+         :on-click #(println name)}
+     [:path {:d "M0,0 L8.35795057e-07,203.122781 C8.44881404e-07,205.33103 1.78205875,207.12117 3.99814921,207.12117 L29.1475731,207.12117 C31.3556894,207.12117 33.1457214,205.330596 33.1457214,203.122781 L33.1457206,0 L0,0 Z"}]]))
 
 (defn white-key [start-key key]
-  [:g {:transform (str "translate(" (* (- key start-key) white-width) ")")
-       :stroke "#000000" :stroke-width "1" :fill "#FFFFFF"
-       :on-click #(println (white-key-name key))}
-   [:path {:d "M0,324.998255 C0,327.760984 2.24331036,330.000618 5.00408559,330.000618 L64.1897584,330.000618 C66.9534386,330.000618 69.193844,327.761793 69.193844,324.998255 L69.193844,0 L0,0 L0,324.998255 Z"}]])
+  (let [name (white-key-name key)
+        x-off (* (- key start-key) white-width)]
+    [:g {:key name
+         :transform (str "translate(" x-off ")")
+         :stroke "#000000" :stroke-width "1" :fill "#FFFFFF"
+         :on-click #(println name)}
+     [:path {:d "M0,324.998255 C0,327.760984 2.24331036,330.000618 5.00408559,330.000618 L64.1897584,330.000618 C66.9534386,330.000618 69.193844,327.761793 69.193844,324.998255 L69.193844,0 L0,0 L0,324.998255 Z"}]]))
 
 (defn popup [start-key key key-name]
   [:g {:transform (str "translate(" (* (- key start-key) white-width) ",-81)")
